b0c22a6f94c0920e4de6022f3ace1a3c16868c54
Edwin Roman <romanseberino@gmail.com>
2023-12-31 01:04:01 -0400

    Working progress page
    This page it is share page to indicate to the users when our team is working in certain part of the web page

---
+++ src/pages/shared/WorkingInProgressPage.tsx
@@ -0,0 +1,19 @@
+
+
+function WorkingInProgressPage() {
+  return (
+    <section className="section is-medium has-text-centered ">
+        <h2 className="title">
+          💫
+        </h2>
+        <h2 className="title">
+          ¡Página bajo mantenimiento!
+        </h2>
+        <h3 className="mt-4 is-italic	is-size-5	">
+          "Estamos trabajando lo más pronto posible para disponibilidad de esta sección, gracias por su consulta!"
+        </h3>
+    </section>
+  )
+}
+
+export default WorkingInProgressPage
