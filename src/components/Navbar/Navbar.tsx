a44b6e5e675296873db35ee2734476a4c695d547
Edwin Roman <romanseberino@gmail.com>
2024-01-01 13:56:47 -0400

    Mobile and Tablet screens bug fixed with the menu when starting the page
    Mobile and Tablet screens bug fixed with the menu when starting the page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,9 +7,9 @@ import NavbarBrand from './NavbarBrand';
 function Navbar() {
 
   const [showMenu, setShowMenu] = useState<boolean>(false);
-
+  
   return (
-    <nav className="navbar is-info navbar is-fixed-top p-3" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-info is-fixed-top p-3" role="navigation" aria-label="main navigation">
         <NavbarBrand toggleMenuAction={()=> { setShowMenu(!showMenu)} } />
         <NavbarMenu showMenu={showMenu} />
     </nav>
@@ -26,7 +26,13 @@ const NavbarMenu : React.FC<INavbarMenuProps> = ({showMenu}) =>{
     const navMenuRef = useRef<HTMLDivElement>(null);
 
     useEffect(()=>{
-        navMenuRef.current?.classList.toggle('is-active');
+
+        if(showMenu){
+           navMenuRef.current?.classList.toggle('is-active');
+        }else{
+           navMenuRef.current?.classList.remove('is-active');
+        }
+        
     },[showMenu])
 
     return(
